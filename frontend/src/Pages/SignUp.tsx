4dd7d259d8ff2eed587bab4334e0985e9f2332bc
Adeniyi F <amf349@cornell.edu>
2023-12-18 00:43:19 -0500

    add backend

---
+++ frontend/src/Pages/SignUp.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function SignUp() {
+  return (
+    <div className="container mt-5">
+      <h2>Sign Up</h2>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" className="form-control" id="password" />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
